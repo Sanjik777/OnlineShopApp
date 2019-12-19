c25c84b87525e18ef56cd797a5224231a62614d5
Sanjikbs <Sanjikbs@gmail.com>
2019-12-19 16:31:28 +0600

    v1

---
+++ OnlineShop/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Services;
+
+namespace OnlineShop.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService userService;
+        private readonly TwilioSmsService twilioSmsService;
+
+        public AuthController(UserService userService, TwilioSmsService twilioSmsService)
+        {
+            this.userService = userService;
+            this.twilioSmsService = twilioSmsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Auth(string phoneNumber, string code)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var token = await userService.Authenticate(phoneNumber, code);
+
+            if (String.IsNullOrEmpty(token))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { token });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendCode(string phoneNumber)
+        {
+            var code = new Random().Next(1000, 9999).ToString();
+
+            await twilioSmsService.SendVerificationCode(phoneNumber, code);
+
+            await userService.SaveCodeToUser(phoneNumber, code);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ OnlineShop/Startup.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using OnlineShop.DataAccess;
+using OnlineShop.Services;
+using OnlineShop.Services.Interfaces;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace OnlineShop
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<UserService>();
+            services.AddTransient<ISmsService, TwilioSmsService>();
+            services.AddDbContext<OnlineShopContext>(options => options.UseNpgsql(configuration.GetConnectionString("DeveloperDatabase")));
+            
+            //JWT
+            //var secrets = configuration.GetSection("Secrets");
+            //var key = Encoding.ASCII.GetBytes(secrets.GetValue<string>("JWTSecret"));
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer(x =>
+            //{
+            //    x.RequireHttpsMetadata = false;
+            //    x.SaveToken = true;
+            //    x.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(key),
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false
+            //    };
+            //});
+
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+			});
+		}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
+
+
+			app.UseRouting();
+
+            app.UseAuthentication();
+
+			//app.UseEndpoints(endpoints =>
+			//{
+			//    endpoints.MapGet("/", async context =>
+			//    {
+			//        await context.Response.WriteAsync("Hello World!");
+			//    });
+			//});
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+    }
+}
